475d45ae4fbdbc5b8313dcd240ecac47ba941d24
Lorenzo <lorenzo.corallo@outlook.it>
2024-07-30 18:55:39 +0200

    chore: format app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default async function Home({
   const query = searchParams?.query;
 
   return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="contaier flex-1 flex flex-col items-center justify-start gap-8 px-4 py-16">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="contaier flex flex-1 flex-col items-center justify-start gap-8 px-4 py-16 w-full">
         <h1 className="text-2xl font-bold">Mom&apos;s Phrases</h1>
-          <Filter />
-          <Suspense key={query} fallback={<p>loading...</p>}>
-            <PhraseList query={query} />
-          </Suspense>
-        </div>
-      </main>
+        <Filter />
+        <Suspense key={query} fallback={<p>loading...</p>}>
+          <PhraseList query={query} />
+        </Suspense>
+      </div>
+    </main>
   );
 }
